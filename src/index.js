754a2461d5d027fb1f0e52671017af351ac9a04b
Gondi László <laszlo.zsolt.gondi@gmail.com>
2022-11-15 08:31:27 +0100

    Make button that shows all the quotes in alphabetical order by their author

--- src/index.js
+++ src/index.js
@@ -1,2 +1,36 @@
-import './style.css';
+//import './style.css';
 
+async function update(){
+    let res = await fetch('../quotes.json')
+    return await res.json();
+}
+
+function printData(data, htmlID){
+    let list = document.getElementById(htmlID);
+    list.textContent = "";
+
+		for (let d of data) {
+			let li = document.createElement("li");
+			li.innerHTML = d.quote+"\n\t-"+d.author;
+			list.appendChild(li);
+		}
+}
+
+document.addEventListener('DOMContentLoaded', ()=>{
+    document.getElementById('all').addEventListener('click',async ()=>{
+        let data = await update();
+        let abc = data.quotes.sort(function(a,b){
+            let first=a.author.toUpperCase();
+            let second = b.author.toUpperCase();
+
+            if (first < second) {
+                return -1;
+            } else if (first > second) {
+                return 1;
+            } else {
+                return 0;
+            }
+        });
+        printData(abc, 'allList');
+    })
+})
