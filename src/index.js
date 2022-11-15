b1ebadaf38fbf9e4eb7059890e7eb8affc7a015c
Gondi László <laszlo.zsolt.gondi@gmail.com>
2022-11-15 08:48:40 +0100

    Make button that filters the quotes so the only ones that show are the ones the include 'the'

--- src/index.js
+++ src/index.js
@@ -1,36 +1,62 @@
-//import './style.css';
+import './style.css';
 
-async function update(){
-    let res = await fetch('../quotes.json')
-    return await res.json();
+async function update() {
+  let res = await fetch("../quotes.json");
+  return await res.json();
 }
 
-function printData(data, htmlID){
-    let list = document.getElementById(htmlID);
-    list.textContent = "";
+function printData(data, htmlID) {
+  let list = document.getElementById(htmlID);
+  list.textContent = "";
 
-		for (let d of data) {
-			let li = document.createElement("li");
-			li.innerHTML = d.quote+"\n\t-"+d.author;
-			list.appendChild(li);
-		}
-}
-
-document.addEventListener('DOMContentLoaded', ()=>{
-    document.getElementById('all').addEventListener('click',async ()=>{
-        let data = await update();
-        let abc = data.quotes.sort(function(a,b){
-            let first=a.author.toUpperCase();
-            let second = b.author.toUpperCase();
-
-            if (first < second) {
-                return -1;
-            } else if (first > second) {
-                return 1;
-            } else {
-                return 0;
+    if(htmlID='theList'){
+        for (let d of data) {
+            let li = document.createElement("li");
+            for(let a of d.quote){
+                if(a==='the' || a === 'The'){
+                    let b = document.createElement("b")
+                    b.innerHTML=a
+                    li.appendChild(b)
+                }else{
+                    li.textContent+=a
+                }
             }
-        });
-        printData(abc, 'allList');
-    })
-})
+            list.appendChild(li);
+          }
+    }else{
+        for (let d of data) {
+          let li = document.createElement("li");
+          li.innerHTML = d.quote + "\n\t-" + d.author;
+          list.appendChild(li);
+        }
+    }
+
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  document.getElementById("all").addEventListener("click", async () => {
+    let data = await update();
+    let abc = data.quotes.sort(function (a, b) {
+      let first = a.author.toUpperCase();
+      let second = b.author.toUpperCase();
+
+      if (first < second) {
+        return -1;
+      } else if (first > second) {
+        return 1;
+      } else {
+        return 0;
+      }
+    });
+    printData(abc, "allList");
+  });
+
+  document.getElementById("the").addEventListener("click", async () => {
+    let theArr = [];
+    let data = await update();
+    let filter = data.quotes.filter(
+      (q) => q.quote.includes("the") || q.quote.includes("The")
+    );
+    printData(filter, "theList");
+  });
+});
